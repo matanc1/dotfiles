d8c61b6ed89d58cb27b475d546f718c26913f5f7
matan <matanc1@Matans-MBP.lan>
2022-11-15 17:28:25 +0200

    added zsh autocompletion and mac support

--- setup/system_requirements.sh
+++ setup/system_requirements.sh
@@ -1,5 +1,17 @@
-sudo apt update -y
-sudo apt install -y git
-sudo apt install -y vim
-sudo apt install -y zsh
-sudo apt install -y python3-pip
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt update -y
+    sudo apt install -y git
+    sudo apt install -y vim
+    sudo apt install -y zsh
+    sudo apt install -y python3-pip
+    sudo apt install -y fzf
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew update
+    brew upgrade
+    brew install git
+    brew install vim
+    brew install zsh
+    brew install fzf
+fi
